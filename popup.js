fe5e0d881cd246cd1e0bf844320dbdbf57134db0
Pravesh <77372438+pravesh0711@users.noreply.github.com>
2025-09-11 17:24:20 +0000

    Time attched

--- popup.js
+++ popup.js
@@ -1,1 +1,23 @@
 console.log("Hello extention!!")
+let startTime;
+
+let time = new Date();
+console.log({time})
+
+function startTimer() {
+    // startTime = Date.now();
+    startTime = new Date()
+
+    setInterval(updateTimer, 1); 
+    // updateTimer();
+
+}
+
+function updateTimer() {
+    // const currentTime = Date.now();
+    const currentTime = new Date()
+    const milliseconds = currentTime
+    document.getElementById("item1").textContent = milliseconds; // Display milliseconds in the div
+}
+
+startTimer();
