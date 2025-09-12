4f1266f1525a1f212ac630f3e2220fd969cbd6bb
Pravesh <77372438+pravesh0711@users.noreply.github.com>
2025-09-12 00:53:45 +0000

    Test converter

--- popup.js
+++ popup.js
@@ -1,29 +1,27 @@
-console.log("Hello extention!!")
 let startTime;
 
 let time = new Date();
 console.log({time})
 
 function startTimer() {
-    // startTime = Date.now();
     startTime = new Date()
-
     setInterval(updateTimer, 1); 
-    // updateTimer();
-
 }
 
 function updateTimer() {
-    // const currentTime = Date.now();
     const currentTime = new Date()
     const milliseconds = {
         ISTtime: currentTime.toLocaleString(),
-        ESTtime: currentTime.toLocaleString("en-US", {timeZone: "America/New_York"}),
-       
+        ESTtime: currentTime.toLocaleString("en-US", {timeZone: "America/New_York"}), 
     }
-    
     document.getElementById("ISTtime").textContent = "IST Time: " +  milliseconds.ISTtime; 
     document.getElementById("ESTtime").textContent = "EST Time: " +  milliseconds.ESTtime;  // Display milliseconds in the div
 }
 
+
+
 startTimer();
+
+
+
+
